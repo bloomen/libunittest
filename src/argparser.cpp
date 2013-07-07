f1dd67c47017b89d6f70cc03eb42e2ec8f8c3e2b
Christian <chr.blume@gmail.com>
2013-07-07 12:58:34 +0200

    simplified parse()

--- src/argparser.cpp
+++ src/argparser.cpp
@@ -18,6 +18,7 @@ struct implementation<argparser> {
         stream << "-h          Displays this help message\n";
         stream << "-v          Sets verbose output for running tests\n";
         stream << "-s          Stops running tests after the first test fails\n";
+        stream << "-e          Turns off exception handling\n";
         stream << "-x          Enables the generation of the XML output\n";
         stream << "-f filter   Specifies a filter applied to the beginning of the test names\n";
         stream << "-t test     Specifies a certain test to run superseding the name filter\n";
@@ -53,20 +54,8 @@ void argparser::parse(int argc, char **argv)
             failure_stop(true);
         } else if (value=="-x") {
             generate_xml(true);
-        } else if (value=="-sv" || value=="-vs") {
-            verbose(true);
-            failure_stop(true);
-        } else if (value=="-vx" || value=="-xv") {
-            verbose(true);
-            generate_xml(true);
-        } else if (value=="-sx" || value=="-xs") {
-            failure_stop(true);
-            generate_xml(true);
-        } else if (value=="-vsx" || value=="-svx" || value=="-xvs" ||
-                   value=="-xsv" || value=="-vxs" || value=="-sxv") {
-            verbose(true);
-            failure_stop(true);
-            generate_xml(true);
+        } else if (value=="-e") {
+            handle_exceptions(false);
         } else if (value=="-f") {
             if (++i<argc) {
                 name_filter(argv[i]);
