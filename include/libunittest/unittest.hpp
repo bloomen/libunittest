40cd8e5e2d632873605b939b3972675ed99fe6f1
Christian <chr.blume@gmail.com>
2013-08-04 21:31:36 +0200

    initial

---
+++ include/libunittest/unittest.hpp
@@ -0,0 +1,24 @@
+/**
+ * @brief A portable C++ library for unit testing,
+ *        http://sourceforge.net/projects/libunittest
+ * @file unittest.hpp
+ */
+#pragma once
+#include <libunittest/argparser.hpp>
+#include <libunittest/assertions.hpp>
+#include <libunittest/call.hpp>
+#include <libunittest/environments.hpp>
+#include <libunittest/formatting.hpp>
+#include <libunittest/testcase.hpp>
+#include <libunittest/testfailure.hpp>
+#include <libunittest/testlog.hpp>
+#include <libunittest/testresults.hpp>
+#include <libunittest/testrun.hpp>
+#include <libunittest/teststatus.hpp>
+#include <libunittest/testsuite.hpp>
+#include <libunittest/userargs.hpp>
+#include <libunittest/utilities.hpp>
+#include <libunittest/version.hpp>
+#include <libunittest/random.hpp>
+#include <libunittest/strings.hpp>
+#include <libunittest/macros.hpp>
