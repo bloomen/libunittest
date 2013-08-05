68d96467dd94d020294d6bd581ddbdec6bcc899e
Christian <chr.blume@gmail.com>
2013-08-05 20:19:04 +0200

    add assertions to each test

--- examples/random/test_random.cpp
+++ examples/random/test_random.cpp
@@ -7,6 +7,8 @@ void test_random::test_random_int()
     random.seed(42); // let's you set the random seed (default is 1)
     auto rand_value1 = random.value(); // a random int in [0, 10]
     auto rand_value2 = random.value(); // another random int [0, 10]
+    assert_in_range(rand_value1, -1, 11, SPOT);
+    assert_in_range(rand_value2, -1, 11, SPOT);
 }
 
 void test_random::test_random_bool()
@@ -14,6 +16,9 @@ void test_random::test_random_bool()
     auto random = unittest::make_random_bool();
     auto rand_value1 = random.value(); // a random bool in [true, false]
     auto rand_value2 = random.value(); // another random bool in [true, false]
+    const std::vector<bool> container = {true, false};
+    assert_in_container(rand_value1, container, SPOT);
+    assert_in_container(rand_value2, container, SPOT);
 }
 
 void test_random::test_random_double()
@@ -21,12 +26,15 @@ void test_random::test_random_double()
     auto random = unittest::make_random_value<double>(2, 5);
     auto rand_value1 = random.value(); // a random double in [2, 5)
     auto rand_value2 = random.value(); // another random double in [2, 5)
+    assert_in_range(rand_value1, 1.9, 5, SPOT);
+    assert_in_range(rand_value2, 1.9, 5, SPOT);
 }
 
 void test_random::test_random_choice()
 {
     auto random = unittest::make_random_choice(unittest::letters_lower);
     auto rand_value = random.value(); // a random, lower-case letter
+    assert_in_container(rand_value, unittest::letters_lower, SPOT);
 }
 
 void test_random::test_random_vector()
@@ -35,6 +43,10 @@ void test_random::test_random_vector()
     auto random2 = unittest::make_random_vector(random1, 10);
     auto rand_vector = random2.value(); // a vector of size 10 filled with
                                         // random doubles from the interval of [0, 1)
+    assert_equal<unsigned>(10, rand_vector.size(), SPOT);
+    for (auto& value : rand_vector) {
+        assert_in_range(value, -0.1, 1, SPOT);
+    }
 }
 
 void test_random::test_random_shuffle()
@@ -42,4 +54,8 @@ void test_random::test_random_shuffle()
     const std::vector<int> container = {1, 2, 3, 4};
     auto random = unittest::make_random_shuffle(container, 2);
     auto rand_vector = random.value(); // a randomly shuffled vector of size 2
+    assert_equal<unsigned>(2, rand_vector.size(), SPOT);
+    for (auto& value : rand_vector) {
+        assert_in_container(value, container, SPOT);
+    }
 }
