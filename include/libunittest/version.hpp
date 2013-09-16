4b0d472abc82b699887261318a3149712a6619c2
Christian <chr.blume@gmail.com>
2013-09-16 20:23:21 +0200

    add version file

---
+++ include/libunittest/version.hpp
@@ -0,0 +1,16 @@
+/**
+ * @brief Auto-generated version file
+ * @file version.hpp
+ */
+#pragma once
+#include <string>
+/**
+ * @brief Unit testing in C++
+ */
+namespace unittest {
+/**
+ * @brief The libunittest version
+ */
+const std::string version = "3.4.1";
+
+} // unittest
