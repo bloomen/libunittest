7b122d63d764cd922e218ff35f955354116314f7
Christian <chr.blume@gmail.com>
2013-09-29 23:37:03 +0200

    safer upload script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 version=$1
 if [ "x$version" = "x" ];then
-    echo "Error: No version given"
-    exit 1
-fi
+
+sftp bloomen,libunittest@web.sourceforge.net << EOF
+cd htdocs
+put doc/web/*.html
+cd css
+put doc/web/css/*.css
+EOF
+exit $?
+
+else
 
 sftp bloomen,libunittest@web.sourceforge.net << EOF
 cd htdocs
@@ -16,3 +23,5 @@ cd /home/frs/project/libunittest
 put distribution/libunittest-${version}.tar.gz
 EOF
 exit $?
+
+fi
