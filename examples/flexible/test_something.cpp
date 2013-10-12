12b0db3d996b43ff67bc8027e55183429dfe19b5
Christian <chr.blume@gmail.com>
2013-10-12 17:30:59 +0200

    no need for header files

--- examples/flexible/test_something.cpp
+++ examples/flexible/test_something.cpp
@@ -1,24 +1,53 @@
-#include "test_something.h"
-#define SPOT UNITTEST_SPOT
+#include <libunittest/unittest.hpp>
+#include <libunittest/shortcuts.hpp>
+
+// a test context
+struct test_context {
+    std::string message;
+    test_context()
+        : message("here I am")
+    {
+        // do something expensive here giving the same context to selected tests
+    }
+    ~test_context()
+    {
+        // shut down what you've done in the constructor
+    }
+};
+
+// a test class with a test context
+struct test_something : unittest::testcase<test_context> {
+
+    // this static method runs the tests
+    static void run()
+    {
+        UNITTEST_CLASS(test_something)
+        test_context context; // context can be shared across tests
+        UNITTEST_RUNCXT(context, test_has_context)
+        UNITTEST_RUNCXT_TIME(context, test_correct_message, 3.5) // a timeout of 3.5s
+        UNITTEST_RUN(test_no_context)
+    }
+
+    void test_has_context()
+    {
+        // in case the test is run with a context, you can always
+        // access the pointer to the context through get_test_context().
+        auto context = get_test_context();
+        assert_true(context, SPOT);
+    }
+
+    void test_correct_message()
+    {
+        auto context = get_test_context();
+        assert_equal("here I am", context->message, SPOT);
+    }
+
+    void test_no_context()
+    {
+        auto context = get_test_context();
+        assert_false(context, SPOT);
+    }
+
+};
 
 UNITTEST_REGISTER(test_something) // this registers the test class
-
-void test_something::test_has_context()
-{
-    // in case the test is run with a context, you can always
-    // access the pointer to the context through get_test_context().
-    auto context = get_test_context();
-    assert_true(context, SPOT);
-}
-
-void test_something::test_correct_message()
-{
-    auto context = get_test_context();
-    assert_equal("here I am", context->message, SPOT);
-}
-
-void test_something::test_no_context()
-{
-    auto context = get_test_context();
-    assert_false(context, SPOT);
-}
--- examples/flexible/test_whatever.cpp
+++ examples/flexible/test_whatever.cpp
@@ -1,21 +1,41 @@
-#include "test_whatever.h"
-#define SPOT UNITTEST_SPOT
+#include <libunittest/unittest.hpp>
+#include <libunittest/shortcuts.hpp>
+
+// a test class without a test context
+struct test_whatever : unittest::testcase<> {
+
+    // this static method runs the tests
+    static void run()
+    {
+        UNITTEST_CLASS(test_whatever)
+        UNITTEST_RUN(test_throw)
+        UNITTEST_RUN(test_in_range)
+        UNITTEST_RUN_TIME(test_in_container, 2) // a timeout of 2s
+    }
+
+    test_whatever() {} // executed before each test
+    ~test_whatever() {} // executed after each test
+
+    void set_up() override {} // executed before each test
+    void tear_down() override {} // executed after each test
+
+    void test_throw()
+    {
+        auto functor = [](){ throw std::bad_cast(); };
+        assert_throw<std::bad_cast>(functor, SPOT);
+    }
+
+    void test_in_range()
+    {
+        assert_in_range(1, 0.9, 1.1, SPOT);
+    }
+
+    void test_in_container()
+    {
+        const std::vector<int> container = {1, 2, 3};
+        assert_in_container(2, container, SPOT);
+    }
+
+};
 
 UNITTEST_REGISTER(test_whatever) // this registers the test class
-
-void test_whatever::test_throw()
-{
-    auto functor = [](){ throw std::bad_cast(); };
-    assert_throw<std::bad_cast>(functor, SPOT);
-}
-
-void test_whatever::test_in_range()
-{
-    assert_in_range(1, 0.9, 1.1, SPOT);
-}
-
-void test_whatever::test_in_container()
-{
-    const std::vector<int> container = {1, 2, 3};
-    assert_in_container(2, container, SPOT);
-}
