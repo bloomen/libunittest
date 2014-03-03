f1e1d5f10505d0c55c534135cc7de433efd2b849
Christian <chr.blume@gmail.com>
2014-03-03 06:06:49 +0100

    fixed doc

--- examples/minimal/test_minimal.cpp
+++ examples/minimal/test_minimal.cpp
@@ -6,7 +6,7 @@ TEST(test_value_is_true)
 }
 
 /* Screen output with './unittest.exe -v':
-test_value_is_true::test ... ok
+test_value_is_true::test ... [0.00045s] ok
 
 --------------------------------------------------
 Ran 1 tests in 0.00045s
