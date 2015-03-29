82c7f5426c84484e2ce5b51bc2151824ade5b0a6
Christian <chr.blume@gmail.com>
2015-03-29 13:29:15 -0600

    better upload script

--- upload.sh
+++ upload.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 set -e
-version=$1
-if [ "x$version" = "x" ];then
-
-sftp bloomen,libunittest@web.sourceforge.net << EOF
-cd htdocs
-put doc/web/*
-EOF
-exit $?
-
-else
+tarball=$1
+if [ "x$tarball" = "x" ];then
+    echo "No tarball provided"
+    exit 1
+fi
 
 sftp bloomen,libunittest@web.sourceforge.net << EOF
 cd htdocs
 put doc/web/*
 cd /home/frs/project/libunittest
-put libunittest-${version}.tar.gz
+put $tarball
 EOF
-exit $?
-
-fi
