367b2f6268b3b3dc7d05fdbb64a7bd79d7bed09a
Christian <chr.blume@gmail.com>
2015-04-04 18:35:57 -0600

    explicit definition for msc

--- src/lib/libunittest/noexcept.hpp
+++ src/lib/libunittest/noexcept.hpp
@@ -5,8 +5,7 @@
 #pragma once
 
 #ifdef _MSC_VER
-#include <yvals.h>
-#define UNITTEST_NOEXCEPT _NOEXCEPT
+#define UNITTEST_NOEXCEPT throw()
 #define UNITTEST_NOEXCEPT_FALSE
 #else
 #define UNITTEST_NOEXCEPT noexcept
