1ac019be2de9e247ff63247d08aa6484a194578b
Christian <chr.blume@gmail.com>
2015-04-17 16:37:15 -0600

    adjust test count

--- test/main.cpp
+++ test/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
         return status;
     } else {
         const auto results = unittest::core::testsuite::instance()->get_results();
-        const auto exp_tests = 217;
+        const auto exp_tests = 210;
         if (exp_tests!=results.n_tests) {
             std::cerr << "Expected " << exp_tests << " tests, but got " << results.n_tests << std::endl;
             status = EXIT_FAILURE;
