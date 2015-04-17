44db1e0b1c6c7c22e913b438e8cc509bbbb85e07
Christian <chr.blume@gmail.com>
2015-04-17 16:21:11 -0600

    better doc

--- examples/templates/test_explicit.cpp
+++ examples/templates/test_explicit.cpp
@@ -1,7 +1,7 @@
 #include <libunittest/all.hpp>
 using namespace unittest::assertions;
 
-template<typename T, typename U>
+template<typename T, typename U> // use as many types as desired
 struct templates_explicit : unittest::testcase<> {
 
     static void run()
@@ -11,7 +11,7 @@ struct templates_explicit : unittest::testcase<> {
         UNITTEST_RUN(test_me<long long>)
     }
 
-    template<typename V>
+    template<typename V> // only one type can be used here
     void test_me()
     {
         T a = 1;
