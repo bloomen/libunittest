a4461ecacc6d20ea2f975b5f33dcbf1b78869b1c
Christian Blume <chr.blume@gmail.com>
2015-06-21 21:57:07 -0600

    add assert macros for approx equal

--- src/lib/libunittest/assertmacros.hpp
+++ src/lib/libunittest/assertmacros.hpp
@@ -123,3 +123,87 @@ unittest::assertions::assert_equal(first, second, UNITTEST_SPOT, UNITTEST_CALL(f
  */
 #define UNITTEST_NDASSERT_NOT_EQUAL_MSG(first, second, message) \
 unittest::assertions::assert_equal(first, second, UNITTEST_SPOT, UNITTEST_CALL(first, second, message), UNITTEST_NDAS, message)
+/**
+ * @brief Asserts that two values are approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon.
+ *  Required operators: <, >, -, <<
+ * @param expected The expected value
+ * @param actual The actual value
+ * @param epsilon The epsilon
+ */
+#define UNITTEST_ASSERT_APPROX_EQUAL(expected, actual, epsilon) \
+unittest::assertions::assert_approx_equal(expected, actual, epsilon, UNITTEST_SPOT, UNITTEST_CALL(expected, actual, epsilon))
+/**
+ * @brief Asserts that two values are approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon.
+ *  Required operators: <, >, -, <<
+ * @param expected The expected value
+ * @param actual The actual value
+ * @param epsilon The epsilon
+ * @param message A user defined message
+ */
+#define UNITTEST_ASSERT_APPROX_EQUAL_MSG(expected, actual, epsilon, message) \
+unittest::assertions::assert_approx_equal(expected, actual, epsilon, UNITTEST_SPOT, UNITTEST_CALL(expected, actual, epsilon, message), message)
+/**
+ * @brief Asserts that two values are approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon (non-deadly assertions).
+ *  Required operators: <, >, -, <<
+ * @param expected The expected value
+ * @param actual The actual value
+ * @param epsilon The epsilon
+ */
+#define UNITTEST_NDASSERT_APPROX_EQUAL(expected, actual, epsilon) \
+unittest::assertions::assert_approx_equal(expected, actual, epsilon, UNITTEST_SPOT, UNITTEST_CALL(expected, actual, epsilon), UNITTEST_NDAS)
+/**
+ * @brief Asserts that two values are approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon (non-deadly assertions).
+ *  Required operators: <, >, -, <<
+ * @param expected The expected value
+ * @param actual The actual value
+ * @param epsilon The epsilon
+ * @param message A user defined message
+ */
+#define UNITTEST_NDASSERT_APPROX_EQUAL_MSG(expected, actual, epsilon, message) \
+unittest::assertions::assert_approx_equal(expected, actual, epsilon, UNITTEST_SPOT, UNITTEST_CALL(expected, actual, epsilon, message), UNITTEST_NDAS, message)
+/**
+ * @brief Asserts that two values are not approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon is false.
+ *  Required operators: <, >, -, <<
+ * @param first The first value
+ * @param second The second value
+ * @param epsilon The epsilon
+ */
+#define UNITTEST_ASSERT_APPROX_NOT_EQUAL(first, second, epsilon) \
+unittest::assertions::assert_approx_equal(first, second, epsilon, UNITTEST_SPOT, UNITTEST_CALL(first, second, epsilon))
+/**
+ * @brief Asserts that two values are not approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon is false.
+ *  Required operators: <, >, -, <<
+ * @param first The first value
+ * @param second The second value
+ * @param epsilon The epsilon
+ * @param message A user defined message
+ */
+#define UNITTEST_ASSERT_APPROX_NOT_EQUAL_MSG(first, second, epsilon, message) \
+unittest::assertions::assert_approx_equal(first, second, epsilon, UNITTEST_SPOT, UNITTEST_CALL(first, second, epsilon, message), message)
+/**
+ * @brief Asserts that two values are not approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon is false (non-deadly assertion).
+ *  Required operators: <, >, -, <<
+ * @param first The first value
+ * @param second The second value
+ * @param epsilon The epsilon
+ */
+#define UNITTEST_NDASSERT_APPROX_NOT_EQUAL(first, second, epsilon) \
+unittest::assertions::assert_approx_equal(first, second, epsilon, UNITTEST_SPOT, UNITTEST_CALL(first, second, epsilon), UNITTEST_NDAS)
+/**
+ * @brief Asserts that two values are not approximately equal up to some
+ *  epsilon. The assertion succeeds if |a - b| < epsilon is false (non-deadly assertion).
+ *  Required operators: <, >, -, <<
+ * @param first The first value
+ * @param second The second value
+ * @param epsilon The epsilon
+ * @param message A user defined message
+ */
+#define UNITTEST_NDASSERT_APPROX_NOT_EQUAL_MSG(first, second, epsilon, message) \
+unittest::assertions::assert_approx_equal(first, second, epsilon, UNITTEST_SPOT, UNITTEST_CALL(first, second, epsilon, message), UNITTEST_NDAS, message)
