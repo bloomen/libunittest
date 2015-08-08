de8cb0102b9a9c3c717c79d3b3183b4a039314d0
Christian Blume <chr.blume@gmail.com>
2015-08-08 10:52:57 -0600

    add switches to disable features

--- src/lib/libunittest/all.hpp
+++ src/lib/libunittest/all.hpp
@@ -6,32 +6,32 @@
 #include "unittest.hpp"
 
 /**
- * @brief Set to 0 to not include an automatic main function
+ * @brief Control switch for including an automatic main function
  */
 #ifndef UNITTEST_MAIN_FUNC
 #define UNITTEST_MAIN_FUNC 1
 #endif
 /**
- * @brief Set to 0 to disable assert shortcuts
+ * @brief Control switch for assert shortcuts
  */
 #ifndef UNITTEST_ASSERT_SHORT
 #define UNITTEST_ASSERT_SHORT 1
 #endif
 /**
- * @brief Set to 0 to disable miscellaneous macro shortcuts
+ * @brief Control switch for miscellaneous macro shortcuts
  */
 #ifndef UNITTEST_MACRO_SHORT
 #define UNITTEST_MACRO_SHORT 1
 #endif
 
-#if UNITTEST_MAIN_FUNC==1
+#if !defined(UNITTEST_NO_MAIN_FUNC) && UNITTEST_MAIN_FUNC==1
 #include "main.hpp"
 #endif
 
-#if UNITTEST_ASSERT_SHORT==1
+#if !defined(UNITTEST_NO_ASSERT_SHORT) && UNITTEST_ASSERT_SHORT==1
 #include "assertshortcuts.hpp"
 #endif
 
-#if UNITTEST_MACRO_SHORT==1
+#if !defined(UNITTEST_NO_MACRO_SHORT) && UNITTEST_MACRO_SHORT==1
 #include "shortcuts.hpp"
 #endif
