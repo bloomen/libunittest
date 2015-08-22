8e2b0462eb2370f7d28dbb03562b253bea01c0fa
Christian Blume <chr.blume@gmail.com>
2015-08-21 20:49:18 -0600

    better error msg

--- src/lib/libunittest/checkers.hpp
+++ src/lib/libunittest/checkers.hpp
@@ -52,7 +52,7 @@ check_epsilon(const T& epsilon,
 {
     unittest::core::check_isnan(epsilon, "epsilon", caller, message...);
     if (!(epsilon > epsilon - epsilon)) {
-        const std::string text = "epsilon not greater than zero";
+        const std::string text = "epsilon not greater than zero" + unittest::core::str_if(": ", epsilon);
         unittest::fail(caller, text, message...);
     }
 }
@@ -75,7 +75,7 @@ check_range_bounds(const T& lower,
     unittest::core::check_isnan(lower, "lower bound", caller, message...);
     unittest::core::check_isnan(upper, "upper bound", caller, message...);
     if (lower > upper) {
-        const std::string text = "lower bound cannot be greater than upper bound";
+        const std::string text = "lower bound cannot be greater than upper bound"  + unittest::core::str_if(": ", lower, ": lower")  + unittest::core::str_if(" > ", upper, " > upper");
         unittest::fail(caller, text, message...);
     }
 }
